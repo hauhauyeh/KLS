8f83f64558b38e18ad912220eab56c4113bfd430
Rajni <patelrajni31@gmail.com>
2025-10-15 18:31:05 +0530

    Create skeleton for Item module.

--- KLS.Data/DataContext/KLSDBContext.cs
+++ KLS.Data/DataContext/KLSDBContext.cs
@@ -69,6 +69,7 @@ namespace KLS.Data.DataContext
             modelBuilder.Entity<TempPayrollService>().ToTable("TempPayrollService");
             modelBuilder.Entity<PayrollDetail>().ToTable("PayrollDetail");
             modelBuilder.Entity<CustomerPayment>().ToTable("CustomerPayment");
+            modelBuilder.Entity<Item>().ToTable("Item");
 
             modelBuilder.Entity<EmpJob>().ToTable("EmpJob");
             modelBuilder.Entity<EmpJob>().Property(c => c.JobCode).ValueGeneratedNever();
@@ -162,6 +163,8 @@ namespace KLS.Data.DataContext
         public DbSet<TempPayrollDetail> TempPayrollDetails { get; set; }
 
         public DbSet<CustomerPayment> CustomerPayments { get; set; }
+
+        public DbSet<Item> Items { get; set; }
 
         #endregion
 
---
+++ KLS.Data/Repositories/ItemRepository.cs
@@ -0,0 +1,20 @@
+﻿using KLS.Contract.Interfaces;
+using KLS.Data.DataContext;
+using KLS.Data.Repositories;
+using KLS.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KLS.Data.Repositories
+{
+    public class ItemRepository : KLSRepository<Item>, IItemRepository
+    {
+        public ItemRepository(KLSDBContext dbContext) : base(dbContext)
+        {
+
+        }
+    }
+}
