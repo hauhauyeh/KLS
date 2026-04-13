532a59ce0deba7ffdbd2c72fa449f1c1ebb2474b
Rajni <patelrajni31@gmail.com>
2026-04-13 12:40:31 +0530

    Fix the customer payment service issue

--- KLS.Services/CustomerPaymentService.cs
+++ KLS.Services/CustomerPaymentService.cs
@@ -6,6 +6,7 @@ using Microsoft.EntityFrameworkCore;
 using Square;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
